dfa939ebfa84f382caed8409c1e0ce1bc541bbd3
Emeka Emodi <emodiemeka@gmail.com>
2023-10-24 05:00:59 +0000

    feat: Implement hypermedia pagination and update docstrings

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+"""
+Hypermedia Pagination
+
+This script defines a Server class for paginating a database
+of popular baby names and provides a method to retrieve hypermedia
+information for a specified page and page size.
+"""
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Calculate the start and end indexes for a given page and page size.
+
+    This function takes two integer arguments: page and page_size. It returns
+    a tuple containing the start and end indexes for the range of elements
+    to be displayed on a particular page.
+
+    Args:
+        page (int): The page number (1-indexed).
+        page_size (int): The number of items per page.
+
+    Returns:
+        Tuple[int, int]: A tuple (start_index, end_index) that reps the range
+        of indexes to return in a list for the given pagination parameters.
+    """
+    if page < 1 or page_size < 1:
+        raise ValueError("Both page and page_size must be positive integers.")
+
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return start_index, end_index
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """Retrieves information about a page.
+        """
+        page_data = self.get_page(page, page_size)
+        start, end = index_range(page, page_size)
+        total_pages = math.ceil(len(self.__dataset) / page_size)
+        page_info = {
+            'page_size': len(page_data),
+            'page': page,
+            'data': page_data,
+            'next_page': page + 1 if end < len(self.__dataset) else None,
+            'prev_page': page - 1 if start > 0 else None,
+            'total_pages': total_pages,
+        }
+        return page_info
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get a page of data from the dataset.
+
+        Args:
+            page (int): The page number (1-indexed).
+            page_size (int): The number of items per page.
+
+        Returns:
+            List[List]: The requested page of data from the dataset.
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        dataset = self.dataset()
+        start_index, end_index = index_range(page, page_size)
+        if start_index <= len(dataset):
+            return dataset[start_index:end_index]
+        else:
+            return []
