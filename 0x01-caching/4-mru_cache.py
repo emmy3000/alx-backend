fe87211e09d44d25227aa167c8ebf856a9aec9ab
Emeka Emodi <emodiemeka@gmail.com>
2023-10-26 04:04:30 +0000

    feat: Add MRUCache class for Most Recently Used (MRU) caching.

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+A Most Recently Used (MRU) caching module.
+"""
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    Represents an object for storing and
+    retrieving items from a dictionary with an MRU
+    removal mechanism when the limit is reached.
+    """
+
+    def __init__(self):
+        """
+        Initializes the cache.
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        Add an item to the cache.
+
+        Args:
+            key: A unique identifier for the item.
+            item: The item to be stored in the cache.
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                mru_key, _ = self.cache_data.popitem(last=False)
+                print("DISCARD:", mru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieve an item by key.
+
+        Args:
+            key: A unique identifier for the item.
+
+        Returns:
+            The item associated with the provided key if found;
+            otherwise, None.
+        """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
