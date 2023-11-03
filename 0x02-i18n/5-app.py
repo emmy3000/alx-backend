c17bdb6416e9b0c8243a6e66629a133b391e90be
Emeka Emodi <emodiemeka@gmail.com>
2023-11-03 21:50:01 +0000

    Add Flask app with Babel i18n and user login emulation

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -4,6 +4,7 @@ A Flask app with Babel i18n configuration and user login emulation.
 """
 from flask import Flask, render_template, request, g
 from flask_babel import Babel, gettext
+from typing import Union, Dict
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -30,7 +31,7 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-def get_user(user_id):
+def get_user(user_id) -> Union[Dict, None]:
     """Retrieve a user from the mock user database.
     Args:
         user_id (int): The ID of the user to retrieve.
@@ -41,11 +42,32 @@ def get_user(user_id):
 
 
 @app.before_request
-def before_request():
+def before_request() -> None:
     """Execute before all other functions to find and set a user in flask.g.
     """
     user_id = request.args.get('login_as', type=int)
     g.user = get_user(user_id)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Determine the best-matching language for the user based on their
+    accepted languages, the available languages configured in the app,
+    and the 'locale' parameter in the request.
+
+    Returns:
+        str: The best-matching language code (e.g., "en" for English).
+    """
+    # Check if 'locale' parameter is present in the request
+    selected_locale = request.args.get('locale')
+
+    if selected_locale in app.config['LANGUAGES']:
+        # If 'locale' parameter is a supported locale, use it
+        return selected_locale
+
+    # Otherwise, use the best-matching language from request.accept_lang
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
