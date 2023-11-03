9cc0b885aeb9c5488cc0ac1011e25fe6b4821bfc
Emeka Emodi <emodiemeka@gmail.com>
2023-11-03 20:04:38 +0000

    feat: Create a basic Flask app with custom welcome message

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""A Basic Flask App with a Custom Welcome Message."""
+from flask import Flask, render_template
+
+# Create a Flask app instance
+app = Flask(__name)
+
+# Make trailing slashes in URLs optional
+app.url_map.strict_slashes = False
+
+
+# Define a route for the home page
+@app.route('/')
+def home():
+    """Render the home page with a custom welcome message."""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    # Run the Flask app on all available network interfaces
+    app.run(host='0.0.0.0', port=5000)
